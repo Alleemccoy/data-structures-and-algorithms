2bedb3721bff65c53343003d189b1e64f791db49
alleemccoy <alleemccoy@hotmail.com>
2021-04-19 21:39:38 -0700

    Finished challenge 2

--- code-challenges/challenges-09.test.js
+++ code-challenges/challenges-09.test.js
@@ -23,7 +23,7 @@ const courseInfo = { name: 'Code 301', duration: { dayTrack: '4 weeks', eveningT
 };
 
 const getCourseKeys = (obj) => {
-  // Solution code here...
+  return Object.entries(obj).map(item => item[0]);
 };
 
 /* ------------------------------------------------------------------------------------------------
